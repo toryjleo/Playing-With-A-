1c81fbf65c7e820362a0485ecbd73eb76622b667
Tory-PC\Tory <tjl1080@rit.edu>
2017-10-23 22:55:54 -0400

    initial commit

---
+++ Playing with A Star/Assets/Scripts/Node.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node> {
+
+	public bool walkable;
+	public Vector3 worldPosition;
+	public int gridX;
+	public int gridY;
+
+	public int gCost;
+	public int hCost;
+	public Node parent;
+
+
+	public Node( bool walkable, Vector3 worldPos, int gridX, int gridY){
+		this.walkable = walkable;
+		worldPosition = worldPos;
+		this.gridX = gridX;
+		this.gridY = gridY;
+	}
+
+	public int fCost {
+		get {
+		return gCost + hCost;
+		}
+	}
+
+	public int HeapIndex {
+		get{
+			return HeapIndex;
+		}
+		set {
+			HeapIndex = value;
+		}
+	}
+
+	public int CompareTo(Node nodeToCompare) {
+		int compare = fCost.CompareTo(nodeToCompare.fCost); 
+		if(compare == 0) {
+			compare = hCost.CompareTo(nodeToCompare.hCost);
+		}
+		return -compare;
+
+	}
+}
